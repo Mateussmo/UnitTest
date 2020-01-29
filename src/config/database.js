e615ebc0b98f32ccf20c5bd2abfb2578435a642a
Mateus Moreira <mateus.m@nagro.app>
2020-01-28 21:50:56 -0300

    Estrutura do Projeto

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host:"127.0.0.1",
+  username:"postgres",
+  password:"1234",
+  database:"algum",
+  dialect:"postgres",
+  operatorsAliases: false,
+  logging:false,
+  define:{
+    timestamps: true,
+    underscored:true,
+    underscoredAll:true,
+  },
+};
