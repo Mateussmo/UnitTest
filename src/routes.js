4e4f7477887a955004acc598e09673ec6105ed16
Mateus Moreira <mateus.m@nagro.app>
2020-02-05 10:28:53 -0300

    feat: nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,4 @@
 const routes = require("express").Router();
-const { User } = require("./app/models");
 
-User.create({name:"Mateus Moreira", email:"mateusmoreirav@gmail.com", password_hash:"123466"});
 
 module.exports = routes;
