f62a4bcba010e6987db5c3828adab89334d83fa4
Mateus Moreira <mateus.m@nagro.app>
2020-02-05 10:25:58 -0300

    feat: Migrations users

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   host:"127.0.0.1",
   username:"postgres",
   password:"1234",
-  database:"algum",
+  database:"unitTest",
   dialect:"postgres",
   operatorsAliases: false,
   logging:false,
