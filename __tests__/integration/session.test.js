e040e014da8f0d1c20d2931b70f336d612cb84d1
Mateus Moreira <mateus.m@nagro.app>
2020-02-06 10:41:06 -0300

    feat: insercao da lib supertest

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,12 +1,12 @@
 const  {User}  = require('../../src/app/models');
 
 describe("Authentication", () => {
-  it("should sum two numbers", async () => {
+  it("should authenticate with valid credentials", async () => {
     const user = await User.create({ name: "mateus",
     email: "mateus@gmail.com",
     password_hash: "123"})
     
-    console.log(user);
+  
     expect(user.email).toBe("mateus@gmail.com");
 
   });
